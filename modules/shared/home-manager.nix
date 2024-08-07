927989bc2681309f647033b968d0d345d2599e8a
Nicolas Buzeta <nicolas@buzeta.net>
2024-08-07 01:24:13 -0400

    add font

--- modules/shared/home-manager.nix
+++ modules/shared/home-manager.nix
@@ -13,4 +13,10 @@
     ./features/zsh
     ./features/btop
   ];
+
+  fonts.fontconfig.enable = true;
+
+  home.packages = with pkgs; [
+    (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
+  ];
 }
