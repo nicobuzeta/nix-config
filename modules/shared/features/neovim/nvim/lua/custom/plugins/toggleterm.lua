613758845b76517ab55d49d501d6821160d1e85c
Nicolas Buzeta <nicolas@buzeta.net>
2024-08-07 12:05:09 -0400

    update nvim

--- modules/shared/features/neovim/nvim/lua/custom/plugins/toggleterm.lua
+++ modules/shared/features/neovim/nvim/lua/custom/plugins/toggleterm.lua
@@ -11,6 +11,15 @@ return {
       float_opts = {
         border = 'double',
       },
+      -- function to run on opening the terminal
+      on_open = function(term)
+        vim.cmd 'startinsert!'
+        vim.api.nvim_buf_set_keymap(term.bufnr, 'n', 'q', '<cmd>close<CR>', { noremap = true, silent = true })
+      end,
+      -- function to run on closing the terminal
+      on_close = function(term)
+        vim.cmd 'startinsert!'
+      end,
     }
     function _lazygit_toggle()
       lazygit:toggle()
