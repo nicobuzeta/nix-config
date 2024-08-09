57aabceef8a3d3145f86ea65dd9a1b271bb29e2c
Nicolas Buzeta <nicolas@buzeta.net>
2024-08-09 00:16:29 -0400

    setup tmux

--- modules/shared/features/tmux/default.nix
+++ modules/shared/features/tmux/default.nix
@@ -2,6 +2,20 @@
 {
   programs.tmux = {
     enable = true;
+    shell = "${pkgs.fish}/bin/fish";
     prefix = "C-a";
+    baseIndex = 1;
+    keyMode = "vi";
+    mouse = true;
+    plugins = with pkgs; [
+      tmuxPlugins.catppuccin
+      tmuxPlugins.sensible
+    ];
+    extraConfig = ''
+      bind b split-window -v
+      bind v split-window -h
+      unbind '"'
+      unbind '%'
+    '';
   };
 }
