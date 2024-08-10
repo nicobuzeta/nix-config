1bc02857a4dc8661c6c9f425500ab3ecfa3c2333
Nicolas Buzeta <nicolas@buzeta.net>
2024-08-09 20:54:11 -0400

    add alacritty

---
+++ modules/shared/features/alacritty/default.nix
@@ -0,0 +1,11 @@
+{ config, pkgs, ... }:
+{
+  programs.alacritty = {
+    enable = true;
+    settings = ''
+      [font.normal]
+      family = "JetBrainsMono Nerd Font Mono"
+      style = "Regular"
+    '';
+  };
+}
