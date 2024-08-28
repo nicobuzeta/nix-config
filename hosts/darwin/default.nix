780ce23fbc7f7865c3d4fa216f759c82591df68f
Nicolas Buzeta <nicolas@buzeta.net>
2024-08-28 10:59:36 -0400

    disable fish system as it overrides homemanager

--- hosts/darwin/default.nix
+++ hosts/darwin/default.nix
@@ -56,8 +56,8 @@ in
   # Add ability to used TouchID for sudo authentication
   security.pam.enableSudoTouchIdAuth = true;
 
-  programs.fish.enable = true;
-  environment.shells = with pkgs; [ fish ];
+  # programs.fish.enable = true;
+  # environment.shells = with pkgs; [ fish ];
 
   # Load configuration that is shared across systems
   environment.systemPackages =
