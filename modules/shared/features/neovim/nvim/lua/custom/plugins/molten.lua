6aab41bbff47075974c3d0e8df54919ba0396eed
Nicolas Buzeta <nicolas@buzeta.net>
2024-09-20 14:42:59 -0300

    working mantain ipynb outputs

--- modules/shared/features/neovim/nvim/lua/custom/plugins/molten.lua
+++ modules/shared/features/neovim/nvim/lua/custom/plugins/molten.lua
@@ -5,7 +5,6 @@ return {
   dependencies = { '3rd/image.nvim' },
   build = ':UpdateRemotePlugins',
   init = function()
-
     -- vim.keymap.set('n', '<localleader>e', ':MoltenEvaluateOperator<CR>', { desc = 'evaluate operator', silent = true })
     vim.keymap.set('n', '<localleader>os', ':noautocmd MoltenEnterOutput<CR>', { desc = 'open output window', silent = true })
     -- vim.keymap.set('n', '<localleader>rr', ':MoltenReevaluateCell<CR>', { desc = 're-eval cell', silent = true })
@@ -69,6 +68,58 @@ return {
     end, {
       nargs = 1,
       complete = 'file',
+    })
+
+    -- Import/export outputs
+    -- automatically import output chunks from a jupyter notebook
+    -- tries to find a kernel that matches the kernel in the jupyter notebook
+    -- falls back to a kernel that matches the name of the active venv (if any)
+    local imb = function(e) -- init molten buffer
+      vim.schedule(function()
+        local kernels = vim.fn.MoltenAvailableKernels()
+        local try_kernel_name = function()
+          local metadata = vim.json.decode(io.open(e.file, 'r'):read 'a')['metadata']
+          return metadata.kernelspec.name
+        end
+        local ok, kernel_name = pcall(try_kernel_name)
+        if not ok or not vim.tbl_contains(kernels, kernel_name) then
+          kernel_name = nil
+          local venv = os.getenv 'VIRTUAL_ENV'
+          if venv ~= nil then
+            kernel_name = string.match(venv, '/.+/(.+)')
+          end
+        end
+        if kernel_name ~= nil and vim.tbl_contains(kernels, kernel_name) then
+          vim.cmd(('MoltenInit %s'):format(kernel_name))
+        end
+        vim.cmd 'MoltenImportOutput'
+      end)
+    end
+
+    -- automatically import output chunks from a jupyter notebook
+    vim.api.nvim_create_autocmd('BufAdd', {
+      pattern = { '*.ipynb' },
+      callback = imb,
+    })
+
+    -- we have to do this as well so that we catch files opened like nvim ./hi.ipynb
+    vim.api.nvim_create_autocmd('BufEnter', {
+      pattern = { '*.ipynb' },
+      callback = function(e)
+        if vim.api.nvim_get_vvar 'vim_did_enter' ~= 1 then
+          imb(e)
+        end
+      end,
+    })
+
+    -- automatically export output chunks to a jupyter notebook on write
+    vim.api.nvim_create_autocmd('BufWritePost', {
+      pattern = { '*.ipynb' },
+      callback = function()
+        if require('molten.status').initialized() == 'Molten' then
+          vim.cmd 'MoltenExportOutput!'
+        end
+      end,
     })
   end,
 }
